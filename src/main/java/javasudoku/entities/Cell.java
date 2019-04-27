2d2ab4691ca53cae324724eb5e4f5cb6254117fe
David312 <davidhs312yt@gmail.com>
2019-04-27 20:00:24 +0200

    Add Cell class

---
+++ src/main/java/javasudoku/entities/Cell.java
@@ -0,0 +1,55 @@
+package javasudoku.entities;
+
+public class Cell {
+  public static final int MIN_VALUE = 0;
+  public static final int MAX_VALUE = 9;
+  public static final int EMPTY_VALUE = 0;
+  
+  private static final String FORMAT = "[%s]";
+
+  private int value;
+
+  public Cell() {
+    this(EMPTY_VALUE);
+  }
+
+  public Cell(int value) {
+    checkValue(value);
+    this.value = value;
+  }
+
+  private void checkValue(int value) {
+    if (value < MIN_VALUE || value > MAX_VALUE) {
+      throw new IllegalArgumentException("Invalid Cell value: " + value);
+    }
+  }
+
+  public int getValue() {
+    return this.value;
+  }
+
+  public void setValue(int value) {
+    checkValue(value);
+    this.value = value;
+  }
+
+  @Override
+  public String toString() {
+    String strValue = this.value != EMPTY_VALUE ? ""+this.value : " ";
+    return String.format(FORMAT, strValue);
+  }
+
+  @Override 
+  public boolean equals(Object o) {
+    if (o == null || !(o instanceof Cell)) {
+      return false;
+    }
+    else if (o == this) {
+      return true;
+    }
+    else {
+      Cell other = (Cell) o;
+      return this.value == other.getValue();
+    }
+  }
+}
---
+++ src/test/java/javasudoku/entities/CellTest.java
@@ -0,0 +1,108 @@
+package javasudoku.entities;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class CellTest {
+  private Cell c;
+ 
+  private Cell createCell() {
+    return new Cell();
+  }
+  private Cell createCell(int value) {
+    return new Cell(value);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void cellWithNegativeValue() {
+    createCell(-1);
+  }  
+
+  @Test(expected = IllegalArgumentException.class)
+  public void cellWithExceedingMaxValue() {
+    createCell(Cell.MAX_VALUE + 1);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void cellWithLoweringMinValue() {
+    createCell(Cell.MIN_VALUE - 1);  
+  }
+
+  @Test
+  public void defaultConstructor() {
+    c = createCell();
+    assertEquals(Cell.EMPTY_VALUE, c.getValue());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void setValueWithBelowMinValue() {
+    c = createCell();
+    c.setValue(Cell.MIN_VALUE - 1);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void setValueWithAboveMaxValue() {
+    c = createCell();
+    c.setValue(Cell.MAX_VALUE + 1);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void setValueWithNegativeValue() {
+    c = createCell();
+    c.setValue(-1);
+  }
+
+  @Test
+  public void setValueValidValue() {
+    c = createCell();
+    c.setValue(Cell.MAX_VALUE - 1);
+    assertEquals(Cell.MAX_VALUE - 1, c.getValue());
+  }
+
+  @Test 
+  public void toStringNonEmptyValue() {
+    c = createCell(Cell.MAX_VALUE);
+    assertEquals(String.format("[%s]", c.getValue()), c.toString());
+  }
+
+  @Test
+  public void toStringEmptyValue() {
+    c = createCell();
+    assertEquals(String.format("[%s]", " "), c.toString());
+  }
+
+  @Test
+  public void equalsWithNull() {
+    c = createCell();
+    assertFalse(c.equals(null));
+  }
+
+  @Test
+  public void equalsWithNonInstanceOfCell() {
+    c = createCell();
+    assertFalse(c.equals(new Object()));
+  }
+
+  @Test
+  public void equalsWithSelf() {
+    c = createCell();
+    assertTrue(c.equals(c));
+  }
+
+  @Test
+  public void equalsWithOtherWithSameValue() {
+    c = createCell();
+    Cell other = createCell();
+    assertTrue(c.equals(other));
+  }
+
+  @Test
+  public void notEqualsWithOtherWithDifferentValue() {
+    c = createCell();
+    Cell other = createCell(Cell.MAX_VALUE);
+    assertFalse(c.equals(other));
+  }
+}
